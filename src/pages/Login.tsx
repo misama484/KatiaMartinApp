4cfd3210cee5660f868d42afd778b327936badff
Miguel Sanchez Martínez <misama484@gmail.com>
2025-03-02 12:39:51 +0100

    reescalado de imagen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,10 +32,10 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-primary py-12 px-4 sm:px-6 lg:px-8">            
+    <div className="min-h-screen flex items-start justify-center bg-primary py-12 px-4 sm:px-6 lg:px-8">            
       <div className="max-w-md w-full space-y-8">
       <div className="flex justify-center">
-          <img className="h-12 w-auto" src={logo2} alt="Logo" />
+          <img className="h-42 w-auto my-6" src={logo2} alt="Logo" />
         </div>
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
